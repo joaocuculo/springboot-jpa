bcb4ee6c664b427f5e04278fff88b95fdd684465
João Cuculo <jv.cuculo@gmail.com>
2026-01-08 20:37:24 -0300

    Create user entity e resource

---
+++ src/main/java/com/aulajava/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.aulajava.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aulajava.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9929293939", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}

005dbd1d7a2a0936df628a01a871ccae3c006e06
João Cuculo <jv.cuculo@gmail.com>
2026-01-08 21:55:18 -0300

    create JPA repository and database seeding

---
+++ src/main/java/com/aulajava/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.aulajava.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aulajava.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
